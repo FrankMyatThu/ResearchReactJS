610279ff48c531c990d17987f39534404468455c
Frank <frank.myatthu@brc.com.sg>
2024-10-06 15:23:32 +0800

    backup

--- research-app/src/App.js
+++ research-app/src/App.js
@@ -1,3 +1,8 @@
+import { Routes, Route, BrowserRouter, Link, Navigate, useNavigate, Outlet} from "react-router-dom";
+import React, { Suspense, lazy, Fragment, useState, useEffect, startTransition } from "react";
+
+import Order from "./View/Order";
+
 import AppHeader from './Layout/Header';
 import AppSideBar from './Layout/SideBar';
 import AppContent from './Layout/Content';
@@ -6,14 +11,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const components = [
+    { path: "/order", component: Order },   
+  ];
+
+  function PrivateRoute({ children }) {    
+    console.log('PrivateRoute rendered');
+    return (      
+        <Fragment>          
+          <AppHeader />
+          <AppSideBar />
+            <div className="App-content" >
+              {children}              
+            </div>
+          <AppFooter />          
+        </Fragment>      
+    );
+  }
+
   return (
-    <div className="App">
-      <AppHeader />
-      <AppSideBar />
-      <AppContent />
-      <AppFooter />      
-    </div>
-  );
+    <Routes>      
+      {
+        components.map((route, index) => (
+          <Route key={index} path={route.path} element={
+            <PrivateRoute> 
+              <route.component />             
+            </PrivateRoute> 
+          } />
+        ))
+      }      
+    </Routes>
+  );  
 }
 
 export default App;
